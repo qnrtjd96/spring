7384f1b350799dba5398127df4e953fba57fb221
leekangsan <qnrtjd96@naver.com>
2021-05-29 22:55:31 +0900

    commit

---
+++ WS06OracleMybaticesNoXml/src/main/java/com/bitcamp/myapp/vo/SearchVO.java
@@ -0,0 +1,21 @@
+package com.bitcamp.myapp.vo;
+
+public class SearchVO {
+	private String searchKey;//검색키
+	private String searchWord;//검색어
+	
+	public String getSearchKey() {
+		return searchKey;
+	}
+	public void setSearchKey(String searchKey) {
+		this.searchKey = searchKey;
+	}
+	public String getSearchWord() {
+		return searchWord;
+	}
+	public void setSearchWord(String searchWord) {
+		this.searchWord = searchWord;
+	}
+	
+	
+}
